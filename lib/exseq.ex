ca9aba6da573e6e8b3d8e183d323fb23e54e9eb9
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-03-08 14:37:19 +0100

    Enough implementation to get accepted as a logger
    Nothing works yet though.

--- lib/exseq.ex
+++ lib/exseq.ex
@@ -1,18 +1,39 @@
 defmodule ExSeq do
-  @moduledoc """
-  Documentation for `Exseq`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
 
-  ## Examples
+  def init(_args) do
+    {:ok, nil}
+  end
 
-      iex> Exseq.hello()
-      :world
+  def handle_event({_level, gl, {Logger, _, _, _}}, state)
+      when node(gl) != node() do
+    {:ok, state}
+  end
 
-  """
-  def hello do
-    :world
+  def handle_event({level, _group_leader, {Logger, message, timestamp, metadata}}, state) do
+    clef_event = %ExSeq.CLEFEvent{
+      timestamp: timestamp,
+      message: message,
+      level: level,
+      properties: metadata
+    }
+
+    IO.inspect(clef_event)
+
+    {:ok, state}
+  end
+
+  def handle_event(:flush, state) do
+    IO.puts "Flush event"
+    {:ok, state}
+  end
+
+  def handle_info(msg, state) do
+    IO.inspect(msg)
+    {:ok, state}
   end
 end
