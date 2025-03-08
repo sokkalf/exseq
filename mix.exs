0450846d66039cea998569e2dcb386be4ff66a7b
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-03-08 10:35:28 +0100

    Add some metadata

--- mix.exs
+++ mix.exs
@@ -6,23 +6,33 @@ defmodule Exseq.MixProject do
       app: :exseq,
       version: "0.1.0",
       elixir: "~> 1.17",
-      start_permanent: Mix.env() == :prod,
+      package: package(),
+      source_url: "https://github.com/sokkalf/exseq",
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  def package do
+    [
+      name: "exseq",
+      description: "Exseq is an Elixir library for logging to Seq",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/sokkalf/exseq"
+      },
+      maintainers: ["sokkalf"],
+      files: ["lib", "mix.exs", "README.md"]
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
